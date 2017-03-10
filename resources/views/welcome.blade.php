6539c00cbc94f07814377a00dcf8170b1a0e3cb8
gosho-kazuya <gosho-kazuya@919.jp>
2017-03-10 18:13:37 +0900

    down to laravel 5.1 clarn installed

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -1,22 +1,48 @@
-<!DOCTYPE html>
-<html lang="en">
+<html>
     <head>
-        <meta charset="utf-8">
-        <meta http-equiv="X-UA-Compatible" content="IE=edge">
-        <meta name="viewport" content="width=device-width, initial-scale=1">
-
         <title>Laravel</title>
 
-        <!-- Fonts -->
-        <link href="https://fonts.googleapis.com/css?family=Raleway:100,600" rel="stylesheet" type="text/css">
+        <link href='//fonts.googleapis.com/css?family=Lato:100' rel='stylesheet' type='text/css'>
 
-        <link href="{{ elixir('css/app.css') }}" rel="stylesheet">
+        <style>
+            body {
+                margin: 0;
+                padding: 0;
+                width: 100%;
+                height: 100%;
+                color: #B0BEC5;
+                display: table;
+                font-weight: 100;
+                font-family: 'Lato';
+            }
+
+            .container {
+                text-align: center;
+                display: table-cell;
+                vertical-align: middle;
+            }
+
+            .content {
+                text-align: center;
+                display: inline-block;
+            }
+
+            .title {
+                font-size: 96px;
+                margin-bottom: 40px;
+            }
+
+            .quote {
+                font-size: 24px;
+            }
+        </style>
     </head>
-
     <body>
-      <div id="app">
-        <example></example>
-      </div>
+        <div class="container">
+            <div class="content">
+                <div class="title">Laravel 5</div>
+                <div class="quote">{{ Inspiring::quote() }}</div>
+            </div>
+        </div>
     </body>
-    <script src="{{ elixir('js/app.js') }}"></script>
 </html>
